88f25f586461f626f4c151af2709ef469603bc14
sdk-team <sdk-team@alibabacloud.com>
2020-09-25 03:46:34 +0000

    Generated python 2020-05-18 for dataworks-public.

--- dataworks-public-20200518/ChangeLog.md
+++ dataworks-public-20200518/ChangeLog.md
@@ -1,3 +1,6 @@
+2020-09-25 Version: 2.0.1
+- Generated python 2020-05-18 for dataworks-public.
+
 2020-09-23 Version: 2.0.0
 - Generated python 2020-05-18 for dataworks-public.
 
--- dataworks-public-20200518/setup.py
+++ dataworks-public-20200518/setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_dataworks-public20200518.
 
-Created on 23/09/2020
+Created on 25/09/2020
 
 @author: Alibaba Cloud SDK
 """
