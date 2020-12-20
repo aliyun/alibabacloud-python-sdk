dbb6975d453d2702dbba2946dffbf7b1efeed350
sdk-team <sdk-team@alibabacloud.com>
2020-12-20 09:24:55 +0000

    Generated python 2019-12-19 for OpenPlatform.

--- openplatform-20191219/ChangeLog.md
+++ openplatform-20191219/ChangeLog.md
@@ -1,3 +1,6 @@
+2020-12-20 Version: 1.1.1
+- Generated python 2019-12-19 for OpenPlatform.
+
 2020-12-18 Version: 1.1.0
 - Supported async function.
 
--- openplatform-20191219/setup.py
+++ openplatform-20191219/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_openplatform20191219.
 
-Created on 18/12/2020
+Created on 20/12/2020
 
 @author: Alibaba Cloud SDK
 """
@@ -37,8 +37,8 @@ AUTHOR_EMAIL = "sdk-team@alibabacloud.com"
 URL = "https://github.com/aliyun/alibabacloud-sdk"
 VERSION = __import__(PACKAGE).__version__
 REQUIRES = [
-    "alibabacloud_tea_util>=0.2.0, <1.0.0",
-    "alibabacloud_tea_rpc>=0.0.6, <1.0.0",
+    "alibabacloud_tea_util>=0.3.1, <1.0.0",
+    "alibabacloud_tea_rpc>=0.1.0, <1.0.0",
     "alibabacloud_endpoint_util>=0.0.3, <1.0.0"
 ]
 
