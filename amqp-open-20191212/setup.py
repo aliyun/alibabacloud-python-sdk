00b0b74d2d427c14249ef0531b873e26f60d59d7
sdk-team <sdk-team@alibabacloud.com>
2020-12-29 02:48:30 +0000

    AMP Version Change.

--- amqp-open-20191212/setup.py
+++ amqp-open-20191212/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_amqp-open20191212.
 
-Created on 28/12/2020
+Created on 29/12/2020
 
 @author: Alibaba Cloud SDK
 """
