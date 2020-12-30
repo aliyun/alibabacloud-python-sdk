8d41b17aa6fd320d6fdf35c821330eaab2367759
sdk-team <sdk-team@alibabacloud.com>
2020-12-30 02:55:04 +0000

    AMP Version Change.

--- ddoscoo-20171228/setup.py
+++ ddoscoo-20171228/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_ddoscoo20171228.
 
-Created on 21/12/2020
+Created on 30/12/2020
 
 @author: Alibaba Cloud SDK
 """
@@ -34,11 +34,11 @@ NAME = "alibabacloud_ddoscoo20171228" or "alibabacloud-package"
 DESCRIPTION = "Alibaba Cloud ddoscoo (20171228) SDK Library for Python"
 AUTHOR = "Alibaba Cloud SDK"
 AUTHOR_EMAIL = "sdk-team@alibabacloud.com"
-URL = "https://github.com/aliyun/alibabacloud-sdk"
+URL = "https://github.com/aliyun/alibabacloud-python-sdk"
 VERSION = __import__(PACKAGE).__version__
 REQUIRES = [
     "alibabacloud_tea_util>=0.3.1, <1.0.0",
-    "alibabacloud_tea_rpc>=0.1.0, <1.0.0",
+    "alibabacloud_tea_openapi>=0.1.0, <1.0.0",
     "alibabacloud_endpoint_util>=0.0.3, <1.0.0"
 ]
 
