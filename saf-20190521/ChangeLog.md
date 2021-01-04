689a4a90f80f56c91d45a80d45d7f644a36ccbe3
sdk-team <sdk-team@alibabacloud.com>
2021-01-04 10:41:04 +0000

    AMP Version Change.

---
+++ saf-20190521/ChangeLog.md
@@ -0,0 +1,3 @@
+2021-01-04 Version: 1.0.0
+- AMP Version Change.
+
---
+++ saf-20190521/alibabacloud_saf20190521/models.py
@@ -0,0 +1,461 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from Tea.model import TeaModel
+from typing import Dict, Any
+
+
+class ExecuteExtendServiceRequest(TeaModel):
+    def __init__(
+        self,
+        region: str = None,
+        service: str = None,
+        service_parameters: str = None,
+    ):
+        self.region = region
+        self.service = service
+        self.service_parameters = service_parameters
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.region is not None:
+            result['Region'] = self.region
+        if self.service is not None:
+            result['Service'] = self.service
+        if self.service_parameters is not None:
+            result['ServiceParameters'] = self.service_parameters
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('Region') is not None:
+            self.region = m.get('Region')
+        if m.get('Service') is not None:
+            self.service = m.get('Service')
+        if m.get('ServiceParameters') is not None:
+            self.service_parameters = m.get('ServiceParameters')
+        return self
+
+
+class ExecuteExtendServiceResponseBodyData(TeaModel):
+    def __init__(
+        self,
+        invoke_result: str = None,
+    ):
+        self.invoke_result = invoke_result
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.invoke_result is not None:
+            result['InvokeResult'] = self.invoke_result
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('InvokeResult') is not None:
+            self.invoke_result = m.get('InvokeResult')
+        return self
+
+
+class ExecuteExtendServiceResponseBody(TeaModel):
+    def __init__(
+        self,
+        message: str = None,
+        request_id: str = None,
+        http_status_code: str = None,
+        data: ExecuteExtendServiceResponseBodyData = None,
+        code: str = None,
+        success: bool = None,
+    ):
+        self.message = message
+        self.request_id = request_id
+        self.http_status_code = http_status_code
+        self.data = data
+        self.code = code
+        self.success = success
+
+    def validate(self):
+        if self.data:
+            self.data.validate()
+
+    def to_map(self):
+        result = dict()
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        if self.http_status_code is not None:
+            result['HttpStatusCode'] = self.http_status_code
+        if self.data is not None:
+            result['Data'] = self.data.to_map()
+        if self.code is not None:
+            result['Code'] = self.code
+        if self.success is not None:
+            result['Success'] = self.success
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        if m.get('HttpStatusCode') is not None:
+            self.http_status_code = m.get('HttpStatusCode')
+        if m.get('Data') is not None:
+            temp_model = ExecuteExtendServiceResponseBodyData()
+            self.data = temp_model.from_map(m['Data'])
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        if m.get('Success') is not None:
+            self.success = m.get('Success')
+        return self
+
+
+class ExecuteExtendServiceResponse(TeaModel):
+    def __init__(
+        self,
+        headers: Dict[str, str] = None,
+        body: ExecuteExtendServiceResponseBody = None,
+    ):
+        self.headers = headers
+        self.body = body
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('body') is not None:
+            temp_model = ExecuteExtendServiceResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
+class ExecuteRequestRequest(TeaModel):
+    def __init__(
+        self,
+        service_parameters: str = None,
+        service: str = None,
+    ):
+        self.service_parameters = service_parameters
+        self.service = service
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.service_parameters is not None:
+            result['ServiceParameters'] = self.service_parameters
+        if self.service is not None:
+            result['Service'] = self.service
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('ServiceParameters') is not None:
+            self.service_parameters = m.get('ServiceParameters')
+        if m.get('Service') is not None:
+            self.service = m.get('Service')
+        return self
+
+
+class ExecuteRequestResponseBody(TeaModel):
+    def __init__(
+        self,
+        message: str = None,
+        request_id: str = None,
+        data: Dict[str, Any] = None,
+        code: int = None,
+    ):
+        self.message = message
+        self.request_id = request_id
+        self.data = data
+        self.code = code
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.code is not None:
+            result['Code'] = self.code
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        return self
+
+
+class ExecuteRequestResponse(TeaModel):
+    def __init__(
+        self,
+        headers: Dict[str, str] = None,
+        body: ExecuteRequestResponseBody = None,
+    ):
+        self.headers = headers
+        self.body = body
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('body') is not None:
+            temp_model = ExecuteRequestResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
+class ExecuteRequestMLRequest(TeaModel):
+    def __init__(
+        self,
+        service_parameters: str = None,
+        service: str = None,
+    ):
+        self.service_parameters = service_parameters
+        self.service = service
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.service_parameters is not None:
+            result['ServiceParameters'] = self.service_parameters
+        if self.service is not None:
+            result['Service'] = self.service
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('ServiceParameters') is not None:
+            self.service_parameters = m.get('ServiceParameters')
+        if m.get('Service') is not None:
+            self.service = m.get('Service')
+        return self
+
+
+class ExecuteRequestMLResponseBody(TeaModel):
+    def __init__(
+        self,
+        message: str = None,
+        request_id: str = None,
+        data: Dict[str, Any] = None,
+        code: int = None,
+    ):
+        self.message = message
+        self.request_id = request_id
+        self.data = data
+        self.code = code
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.code is not None:
+            result['Code'] = self.code
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        return self
+
+
+class ExecuteRequestMLResponse(TeaModel):
+    def __init__(
+        self,
+        headers: Dict[str, str] = None,
+        body: ExecuteRequestMLResponseBody = None,
+    ):
+        self.headers = headers
+        self.body = body
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('body') is not None:
+            temp_model = ExecuteRequestMLResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
+class ExecuteRequestSGRequest(TeaModel):
+    def __init__(
+        self,
+        service_parameters: str = None,
+        service: str = None,
+    ):
+        self.service_parameters = service_parameters
+        self.service = service
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.service_parameters is not None:
+            result['ServiceParameters'] = self.service_parameters
+        if self.service is not None:
+            result['Service'] = self.service
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('ServiceParameters') is not None:
+            self.service_parameters = m.get('ServiceParameters')
+        if m.get('Service') is not None:
+            self.service = m.get('Service')
+        return self
+
+
+class ExecuteRequestSGResponseBody(TeaModel):
+    def __init__(
+        self,
+        message: str = None,
+        request_id: str = None,
+        data: Dict[str, Any] = None,
+        code: int = None,
+    ):
+        self.message = message
+        self.request_id = request_id
+        self.data = data
+        self.code = code
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        if self.message is not None:
+            result['Message'] = self.message
+        if self.request_id is not None:
+            result['RequestId'] = self.request_id
+        if self.data is not None:
+            result['Data'] = self.data
+        if self.code is not None:
+            result['Code'] = self.code
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('Message') is not None:
+            self.message = m.get('Message')
+        if m.get('RequestId') is not None:
+            self.request_id = m.get('RequestId')
+        if m.get('Data') is not None:
+            self.data = m.get('Data')
+        if m.get('Code') is not None:
+            self.code = m.get('Code')
+        return self
+
+
+class ExecuteRequestSGResponse(TeaModel):
+    def __init__(
+        self,
+        headers: Dict[str, str] = None,
+        body: ExecuteRequestSGResponseBody = None,
+    ):
+        self.headers = headers
+        self.body = body
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('body') is not None:
+            temp_model = ExecuteRequestSGResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
