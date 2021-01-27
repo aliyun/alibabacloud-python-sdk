52fc6301a969eca9b8e1a3205b9c33115bc9ba2e
sdk-team <sdk-team@alibabacloud.com>
2021-01-27 10:10:35 +0000

    Generated python 2017-12-14 for BssOpenApi.

--- bssopenapi-20171214/ChangeLog.md
+++ bssopenapi-20171214/ChangeLog.md
@@ -1,3 +1,6 @@
+2021-01-27 Version: 1.0.0
+- Generated python 2017-12-14 for BssOpenApi.
+
 2021-01-19 Version: 0.1.2
 - Generated python 2017-12-14 for BssOpenApi.
 
--- bssopenapi-20171214/setup.py
+++ bssopenapi-20171214/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_bssopenapi20171214.
 
-Created on 19/01/2021
+Created on 27/01/2021
 
 @author: Alibaba Cloud SDK
 """
@@ -38,7 +38,8 @@ URL = "https://github.com/aliyun/alibabacloud-python-sdk"
 VERSION = __import__(PACKAGE).__version__
 REQUIRES = [
     "alibabacloud_tea_util>=0.3.1, <1.0.0",
-    "alibabacloud_tea_rpc>=0.1.0, <1.0.0",
+    "alibabacloud_tea_openapi>=0.2.0, <1.0.0",
+    "alibabacloud_openapi_util>=0.1.1, <1.0.0",
     "alibabacloud_endpoint_util>=0.0.3, <1.0.0"
 ]
 
