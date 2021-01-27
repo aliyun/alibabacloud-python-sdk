925d592be3b4f6c51308c23fd747952b9a7df6fa
sdk-team <sdk-team@alibabacloud.com>
2021-01-27 10:32:51 +0000

    Generated python 2019-01-01 for Cms.

--- cms-20190101/ChangeLog.md
+++ cms-20190101/ChangeLog.md
@@ -1,3 +1,6 @@
+2021-01-27 Version: 2.0.0
+- Generated python 2019-01-01 for Cms.
+
 2021-01-20 Version: 2.0.0
 - Generated python 2019-01-01 for Cms.
 
--- cms-20190101/setup.py
+++ cms-20190101/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_cms20190101.
 
-Created on 20/01/2021
+Created on 27/01/2021
 
 @author: Alibaba Cloud SDK
 """
