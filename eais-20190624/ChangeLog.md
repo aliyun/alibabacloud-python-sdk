d53e355bdc9335752e726e8137a4785ea6075ded
sdk-team <sdk-team@alibabacloud.com>
2021-03-22 08:05:49 +0000

    CreateEai api adds SecurityGroupId, VSwitchId parameters.

--- eais-20190624/ChangeLog.md
+++ eais-20190624/ChangeLog.md
@@ -1,3 +1,6 @@
+2021-03-22 Version: 1.0.1
+- CreateEai api adds SecurityGroupId, VSwitchId parameters.
+
 2021-02-19 Version: 1.0.0
 - Initialization.
 
--- eais-20190624/alibabacloud_eais20190624/models.py
+++ eais-20190624/alibabacloud_eais20190624/models.py
@@ -114,11 +114,15 @@ class CreateEaiRequest(TeaModel):
         instance_type: str = None,
         client_token: str = None,
         instance_name: str = None,
+        security_group_id: str = None,
+        v_switch_id: str = None,
     ):
         self.region_id = region_id
         self.instance_type = instance_type
         self.client_token = client_token
         self.instance_name = instance_name
+        self.security_group_id = security_group_id
+        self.v_switch_id = v_switch_id
 
     def validate(self):
         pass
@@ -133,6 +137,10 @@ class CreateEaiRequest(TeaModel):
             result['ClientToken'] = self.client_token
         if self.instance_name is not None:
             result['InstanceName'] = self.instance_name
+        if self.security_group_id is not None:
+            result['SecurityGroupId'] = self.security_group_id
+        if self.v_switch_id is not None:
+            result['VSwitchId'] = self.v_switch_id
         return result
 
     def from_map(self, m: dict = None):
@@ -145,6 +153,10 @@ class CreateEaiRequest(TeaModel):
             self.client_token = m.get('ClientToken')
         if m.get('InstanceName') is not None:
             self.instance_name = m.get('InstanceName')
+        if m.get('SecurityGroupId') is not None:
+            self.security_group_id = m.get('SecurityGroupId')
+        if m.get('VSwitchId') is not None:
+            self.v_switch_id = m.get('VSwitchId')
         return self
 
 
