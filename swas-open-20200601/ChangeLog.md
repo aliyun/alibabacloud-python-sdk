96839b5fe186ca3a465e48813b69973209c7d72f
sdk-team <sdk-team@alibabacloud.com>
2021-11-23 06:23:37 +0000

    Improve the error code.

--- swas-open-20200601/ChangeLog.md
+++ swas-open-20200601/ChangeLog.md
@@ -1,3 +1,6 @@
+2021-11-23 Version: 1.0.2
+- Improve the error code.
+
 2021-10-27 Version: 1.0.0
 - Support all language SDK.
 
--- swas-open-20200601/setup.py
+++ swas-open-20200601/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_swas-open20200601.
 
-Created on 27/10/2021
+Created on 23/11/2021
 
 @author: Alibaba Cloud SDK
 """
