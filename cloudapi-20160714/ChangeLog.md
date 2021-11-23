7284aaaa127070c5edd974a57bea56abb18a5756
sdk-team <sdk-team@alibabacloud.com>
2021-11-23 03:35:01 +0000

    Update DescribeApisByApp.

--- cloudapi-20160714/ChangeLog.md
+++ cloudapi-20160714/ChangeLog.md
@@ -1,3 +1,6 @@
+2021-11-23 Version: 2.0.0
+- Update DescribeApisByApp.
+
 2021-10-26 Version: 1.0.0
 - Create SDK after migrating from POP to apm.
 
--- cloudapi-20160714/setup.py
+++ cloudapi-20160714/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_cloudapi20160714.
 
-Created on 26/10/2021
+Created on 23/11/2021
 
 @author: Alibaba Cloud SDK
 """
