f2582a47fe5b6b70a10fec23a57f1058dfd3da09
sdk-team <sdk-team@alibabacloud.com>
2021-12-02 03:35:25 +0000

    Timers  to timerType.

--- pts-20201020/ChangeLog.md
+++ pts-20201020/ChangeLog.md
@@ -1,3 +1,6 @@
+2021-12-02 Version: 1.8.4
+- Timers  to timerType.
+
 2021-12-01 Version: 1.8.3
 - Timers  to timerType.
 
--- pts-20201020/setup.py
+++ pts-20201020/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_pts20201020.
 
-Created on 01/12/2021
+Created on 02/12/2021
 
 @author: Alibaba Cloud SDK
 """
