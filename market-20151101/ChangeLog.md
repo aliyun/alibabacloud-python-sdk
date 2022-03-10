8910ab94940a72296f98caf379b78e21b441467c
sdk-team <sdk-team@alibabacloud.com>
2022-03-10 07:36:57 +0000

    Remove private api.

--- market-20151101/ChangeLog.md
+++ market-20151101/ChangeLog.md
@@ -1,3 +1,6 @@
+2022-03-10 Version: 3.0.2
+- Remove private api.
+
 2022-02-08 Version: 3.0.1
 - CreateRate, DescribeRate add packageVersion, customerLabels column.
 
--- market-20151101/setup.py
+++ market-20151101/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_market20151101.
 
-Created on 08/02/2022
+Created on 10/03/2022
 
 @author: Alibaba Cloud SDK
 """
