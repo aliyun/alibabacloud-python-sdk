5d9ffd5441d2e43e8f0780fe8ddfcf044eb11239
sdk-team <sdk-team@alibabacloud.com>
2022-03-24 07:58:45 +0000

    Clickhouse add restore.

--- clickhouse-20191111/setup.py
+++ clickhouse-20191111/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_clickhouse20191111.
 
-Created on 23/03/2022
+Created on 24/03/2022
 
 @author: Alibaba Cloud SDK
 """
