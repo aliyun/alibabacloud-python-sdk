4b909c7b9a17cb0397fbfbaf2ce31a0bf243c132
sdk-team <sdk-team@alibabacloud.com>
2023-03-03 02:42:04 +0000

    DocumentInit.

--- documentautoml-20221229/ChangeLog.md
+++ documentautoml-20221229/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-03-03 Version: 1.0.2
+- DocumentInit.
+
 2023-02-10 Version: 1.0.1
 - DocumentInit.
 
--- documentautoml-20221229/setup.py
+++ documentautoml-20221229/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_documentautoml20221229.
 
-Created on 10/02/2023
+Created on 03/03/2023
 
 @author: Alibaba Cloud SDK
 """
