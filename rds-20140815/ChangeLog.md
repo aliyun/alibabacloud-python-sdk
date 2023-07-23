97d3b9b8b7eccdd464ff3c76e6f1b21e540683d1
sdk-team <sdk-team@alibabacloud.com>
2023-07-23 15:11:06 +0000

    Fix 500 for CreateOrder.

--- rds-20140815/ChangeLog.md
+++ rds-20140815/ChangeLog.md
@@ -1,3 +1,7 @@
+2023-07-23 Version: 2.2.22
+- Fix 500 for CreateOrder.
+- Support sts.
+
 2023-07-22 Version: 2.2.21
 - Fix 500 for CreateOrder.
 - Support sts.
--- rds-20140815/setup.py
+++ rds-20140815/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_rds20140815.
 
-Created on 22/07/2023
+Created on 23/07/2023
 
 @author: Alibaba Cloud SDK
 """
