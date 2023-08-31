9b01bc58a4ea273916bf4459b121a8f46e509738
sdk-team <sdk-team@alibabacloud.com>
2023-08-31 17:14:50 +0000

    Generated python 2019-03-15 for adb.

--- adb-20190315/ChangeLog.md
+++ adb-20190315/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-08-31 Version: 1.1.2
+- Generated python 2019-03-15 for adb.
+
 2023-08-22 Version: 1.1.1
 - Generated python 2019-03-15 for adb.
 
--- adb-20190315/setup.py
+++ adb-20190315/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_adb20190315.
 
-Created on 22/08/2023
+Created on 31/08/2023
 
 @author: Alibaba Cloud SDK
 """
