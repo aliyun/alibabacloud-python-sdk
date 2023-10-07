44ea91bef8360623525196e09255572554cc9a83
sdk-team <sdk-team@alibabacloud.com>
2023-10-07 17:10:45 +0000

    Generated python 2021-02-04 for AIWorkSpace.

--- aiworkspace-20210204/setup.py
+++ aiworkspace-20210204/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_aiworkspace20210204.
 
-Created on 26/09/2023
+Created on 07/10/2023
 
 @author: Alibaba Cloud SDK
 """
