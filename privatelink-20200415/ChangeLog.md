227d81063ca3e568149fab68fd2afdc6bd4c76c9
sdk-team <sdk-team@alibabacloud.com>
2023-11-03 17:28:31 +0000

    Generated python 2020-04-15 for Privatelink.

--- privatelink-20200415/ChangeLog.md
+++ privatelink-20200415/ChangeLog.md
@@ -1,3 +1,6 @@
+2023-11-03 Version: 3.0.1
+- Generated python 2020-04-15 for Privatelink.
+
 2023-09-27 Version: 3.0.0
 - Generated python 2020-04-15 for Privatelink.
 
--- privatelink-20200415/setup.py
+++ privatelink-20200415/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_privatelink20200415.
 
-Created on 27/09/2023
+Created on 03/11/2023
 
 @author: Alibaba Cloud SDK
 """
