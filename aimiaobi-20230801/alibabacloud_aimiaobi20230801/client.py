325ba74dd1b2cc5d07827f03444526f7ee64843b
sdk-team <sdk-team@alibabacloud.com>
2024-01-16 10:29:29 +0000

    Generated python 2023-08-01 for AiMiaoBi.

---
+++ aimiaobi-20230801/alibabacloud_aimiaobi20230801/client.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from typing import Dict
+from Tea.core import TeaCore
+
+from alibabacloud_tea_openapi.client import Client as OpenApiClient
+from alibabacloud_tea_openapi import models as open_api_models
+from alibabacloud_tea_util.client import Client as UtilClient
+from alibabacloud_endpoint_util.client import Client as EndpointUtilClient
+from alibabacloud_aimiaobi20230801 import models as ai_miao_bi_20230801_models
+from alibabacloud_tea_util import models as util_models
+from alibabacloud_openapi_util.client import Client as OpenApiUtilClient
+
+
+class Client(OpenApiClient):
+    """
+    *\
+    """
+    def __init__(
+        self, 
+        config: open_api_models.Config,
+    ):
+        super().__init__(config)
+        self._endpoint_rule = ''
+        self.check_config(config)
+        self._endpoint = self.get_endpoint('aimiaobi', self._region_id, self._endpoint_rule, self._network, self._suffix, self._endpoint_map, self._endpoint)
+
+    def get_endpoint(
+        self,
+        product_id: str,
+        region_id: str,
+        endpoint_rule: str,
+        network: str,
+        suffix: str,
+        endpoint_map: Dict[str, str],
+        endpoint: str,
+    ) -> str:
+        if not UtilClient.empty(endpoint):
+            return endpoint
+        if not UtilClient.is_unset(endpoint_map) and not UtilClient.empty(endpoint_map.get(region_id)):
+            return endpoint_map.get(region_id)
+        return EndpointUtilClient.get_endpoint_rules(product_id, region_id, endpoint_rule, network, suffix)
+
+    def create_token_with_options(
+        self,
+        request: ai_miao_bi_20230801_models.CreateTokenRequest,
+        runtime: util_models.RuntimeOptions,
+    ) -> ai_miao_bi_20230801_models.CreateTokenResponse:
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.agent_key):
+            query['AgentKey'] = request.agent_key
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='CreateToken',
+            version='2023-08-01',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            ai_miao_bi_20230801_models.CreateTokenResponse(),
+            self.call_api(params, req, runtime)
+        )
+
+    async def create_token_with_options_async(
+        self,
+        request: ai_miao_bi_20230801_models.CreateTokenRequest,
+        runtime: util_models.RuntimeOptions,
+    ) -> ai_miao_bi_20230801_models.CreateTokenResponse:
+        UtilClient.validate_model(request)
+        query = {}
+        if not UtilClient.is_unset(request.agent_key):
+            query['AgentKey'] = request.agent_key
+        req = open_api_models.OpenApiRequest(
+            query=OpenApiUtilClient.query(query)
+        )
+        params = open_api_models.Params(
+            action='CreateToken',
+            version='2023-08-01',
+            protocol='HTTPS',
+            pathname='/',
+            method='POST',
+            auth_type='AK',
+            style='RPC',
+            req_body_type='formData',
+            body_type='json'
+        )
+        return TeaCore.from_map(
+            ai_miao_bi_20230801_models.CreateTokenResponse(),
+            await self.call_api_async(params, req, runtime)
+        )
+
+    def create_token(
+        self,
+        request: ai_miao_bi_20230801_models.CreateTokenRequest,
+    ) -> ai_miao_bi_20230801_models.CreateTokenResponse:
+        runtime = util_models.RuntimeOptions()
+        return self.create_token_with_options(request, runtime)
+
+    async def create_token_async(
+        self,
+        request: ai_miao_bi_20230801_models.CreateTokenRequest,
+    ) -> ai_miao_bi_20230801_models.CreateTokenResponse:
+        runtime = util_models.RuntimeOptions()
+        return await self.create_token_with_options_async(request, runtime)
