bd7bfee1c96dc90a4cd641139f906cdb41ae106a
sdk-team <sdk-team@alibabacloud.com>
2024-02-04 17:15:07 +0000

    Generated python 2020-03-09 for Eipanycast.

--- eipanycast-20200309/setup.py
+++ eipanycast-20200309/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_eipanycast20200309.
 
-Created on 25/01/2024
+Created on 04/02/2024
 
 @author: Alibaba Cloud SDK
 """
