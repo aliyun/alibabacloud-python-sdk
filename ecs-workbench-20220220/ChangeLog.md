d64853e321aa6f9c9a7f8609bcbb8ec03fd1ee57
sdk-team <sdk-team@alibabacloud.com>
2024-02-19 09:21:50 +0000

    Generated python 2022-02-20 for ecs-workbench.

--- ecs-workbench-20220220/ChangeLog.md
+++ ecs-workbench-20220220/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-02-19 Version: 1.0.2
+- Generated python 2022-02-20 for ecs-workbench.
+
 2024-02-05 Version: 1.0.1
 - Update API LoginInstanceupdate InstanceLoginInfo param.
 
--- ecs-workbench-20220220/setup.py
+++ ecs-workbench-20220220/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_ecs-workbench20220220.
 
-Created on 05/02/2024
+Created on 19/02/2024
 
 @author: Alibaba Cloud SDK
 """
