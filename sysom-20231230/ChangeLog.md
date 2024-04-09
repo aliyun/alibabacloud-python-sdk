cc2c3470cb9fd945aedbef1d995c6472cf67abdf
sdk-team <sdk-team@alibabacloud.com>
2024-04-09 07:25:58 +0000

    Generated python 2023-12-30 for SysOM.

--- sysom-20231230/ChangeLog.md
+++ sysom-20231230/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-04-09 Version: 1.0.1
+- Generated python 2023-12-30 for SysOM.
+
 2024-01-29 Version: 1.0.0
 - Generated python 2023-12-30 for SysOM.
 
--- sysom-20231230/setup.py
+++ sysom-20231230/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_sysom20231230.
 
-Created on 29/01/2024
+Created on 09/04/2024
 
 @author: Alibaba Cloud SDK
 """
