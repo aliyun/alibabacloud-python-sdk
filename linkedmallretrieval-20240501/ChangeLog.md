1f5d97d5e487b8f7e79c76ae95dc627b945c2da8
sdk-team <sdk-team@alibabacloud.com>
2024-06-05 02:03:00 +0000

    Generated python 2024-05-01 for LinkedmallRetrieval.

--- linkedmallretrieval-20240501/ChangeLog.md
+++ linkedmallretrieval-20240501/ChangeLog.md
@@ -1,3 +1,6 @@
+2024-06-05 Version: 1.0.2
+- Generated python 2024-05-01 for LinkedmallRetrieval.
+
 2024-05-23 Version: 1.0.1
 - Generated python 2024-05-01 for LinkedmallRetrieval.
 
--- linkedmallretrieval-20240501/setup.py
+++ linkedmallretrieval-20240501/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_linkedmallretrieval20240501.
 
-Created on 23/05/2024
+Created on 05/06/2024
 
 @author: Alibaba Cloud SDK
 """
