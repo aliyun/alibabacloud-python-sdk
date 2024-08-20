306bb7e097464d2ad34355acddd7b57e0ee7726c
sdk-team <sdk-team@alibabacloud.com>
2024-08-20 09:39:39 +0000

    Support API CreateCsr.

--- cas-20200407/alibabacloud_cas20200407/models.py
+++ cas-20200407/alibabacloud_cas20200407/models.py
@@ -3399,10 +3399,13 @@ class GetCsrDetailResponseBody(TeaModel):
     def __init__(
         self,
         csr: str = None,
+        private_key: str = None,
         request_id: str = None,
     ):
         # The content of the CSR.
         self.csr = csr
+        # The private key. Specify a Base64-encoded string.
+        self.private_key = private_key
         # The request ID.
         self.request_id = request_id
 
@@ -3417,6 +3420,8 @@ class GetCsrDetailResponseBody(TeaModel):
         result = dict()
         if self.csr is not None:
             result['Csr'] = self.csr
+        if self.private_key is not None:
+            result['PrivateKey'] = self.private_key
         if self.request_id is not None:
             result['RequestId'] = self.request_id
         return result
@@ -3425,6 +3430,8 @@ class GetCsrDetailResponseBody(TeaModel):
         m = m or dict()
         if m.get('Csr') is not None:
             self.csr = m.get('Csr')
+        if m.get('PrivateKey') is not None:
+            self.private_key = m.get('PrivateKey')
         if m.get('RequestId') is not None:
             self.request_id = m.get('RequestId')
         return self
