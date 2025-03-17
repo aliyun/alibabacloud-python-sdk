df931d9100a1c8b3bac2da67d9005cd47de8bab6
sdk-team <sdk-team@alibabacloud.com>
2025-03-17 03:23:34 +0000

    Generated python 2024-08-23 for DataAnalysisGBI.

--- dataanalysisgbi-20240823/setup.py
+++ dataanalysisgbi-20240823/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_dataanalysisgbi20240823.
 
-Created on 14/03/2025
+Created on 17/03/2025
 
 @author: Alibaba Cloud SDK
 """
