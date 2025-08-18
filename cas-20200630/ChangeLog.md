539cf0b41e5d4d6954e779c7c7f91e75a7cef0f0
sdk-team <sdk-team@alibabacloud.com>
2025-08-18 03:38:00 +0000

    Generated python 2020-06-30 for cas.

--- cas-20200630/ChangeLog.md
+++ cas-20200630/ChangeLog.md
@@ -1,3 +1,6 @@
+2025-08-18 Version: 1.1.6
+- Generated python 2020-06-30 for cas.
+
 2025-08-07 Version: 1.1.5
 - Generated python 2020-06-30 for cas.
 
--- cas-20200630/setup.py
+++ cas-20200630/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_cas20200630.
 
-Created on 07/08/2025
+Created on 18/08/2025
 
 @author: Alibaba Cloud SDK
 """
