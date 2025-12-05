0cbfae47f550f49f3807d65b885fc459b679c33e
sdk-team <sdk-team@alibabacloud.com>
2025-12-05 02:24:57 +0000

    Generated python 2024-05-18 for dataworks-public.

--- dataworks-public-20240518/ChangeLog.md
+++ dataworks-public-20240518/ChangeLog.md
@@ -1,3 +1,6 @@
+2025-12-05 Version: 7.8.1
+- Generated python 2024-05-18 for dataworks-public.
+
 2025-12-05 Version: 7.8.0
 - Support API CreateDataset.
 - Support API CreateDatasetVersion.
--- dataworks-public-20240518/setup.py
+++ dataworks-public-20240518/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_dataworks_public20240518.
 
-Created on 04/12/2025
+Created on 05/12/2025
 
 @author: Alibaba Cloud SDK
 """
