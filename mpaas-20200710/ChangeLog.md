cd3291406a0589f676adf635b39bf096f3c1db68
sdk-team <sdk-team@alibabacloud.com>
2025-12-08 07:47:45 +0000

    Generated python 2020-07-10 for mPaaS.

--- mpaas-20200710/ChangeLog.md
+++ mpaas-20200710/ChangeLog.md
@@ -1,3 +1,6 @@
+2025-12-08 Version: 1.0.1
+- Generated python 2020-07-10 for mPaaS.
+
 2025-08-25 Version: 1.0.0
 - Generated python 2020-07-10 for mPaaS.
 
--- mpaas-20200710/setup.py
+++ mpaas-20200710/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_mpaas20200710.
 
-Created on 25/08/2025
+Created on 08/12/2025
 
 @author: Alibaba Cloud SDK
 """
