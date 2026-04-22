c7a79859822bf20ac19f3622648614246b8b4bf7
sdk-team <sdk-team@alibabacloud.com>
2026-04-22 08:52:42 +0000

    Generated python 2025-11-27 for ThirdswAicall.

--- thirdswaicall-20251127/ChangeLog.md
+++ thirdswaicall-20251127/ChangeLog.md
@@ -1,3 +1,6 @@
+2026-04-22 Version: 1.0.2
+- Generated python 2025-11-27 for ThirdswAicall.
+
 2026-03-05 Version: 1.0.1
 - Generated python 2025-11-27 for ThirdswAicall.
 
--- thirdswaicall-20251127/setup.py
+++ thirdswaicall-20251127/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 """
 setup module for alibabacloud_thirdswaicall20251127.
 
-Created on 05/03/2026
+Created on 22/04/2026
 
 @author: Alibaba Cloud SDK
 """
